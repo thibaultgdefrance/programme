3d6cccca620902dc17fd0954d18a13b5bbe91710
Thibault Defrance <thibault.g.defrance@gmail.com>
2017-01-12 14:16:33 +0100

    -le_carre_de_tom_jedusor

---
+++ intro-javascript/chapter-5/js/course.js
@@ -0,0 +1,18 @@
+var prenom= prompt("entrez votre prenom");
+var nom= prompt("entrez votre nom");
+
+
+
+function direBonjour(prenom, nom) {
+    var message = "Bonjour, " + prenom + " " + nom + " !";
+    alert(message);
+    alert("Je m'appel Tom Jedusor");
+}
+direBonjour(prenom, nom);
+
+var x = prompt("entrez votre nombre");
+function carre(x) {
+    var y= x*x;
+    alert(y);
+}
+carre(x);
